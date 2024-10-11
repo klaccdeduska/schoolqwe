4ad24e334625a9c20adbefc9d2b5eb45b65705a2
klaccdeduska <burov1808@gmail.com>
2024-10-11 13:24:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-.container
+async function getMessages() {
+  const url = "https://api.sampleapis.com/cartoons/cartoons3D";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const element = document.querySelector(".work");
+  element.innerHTML = "";
+
+}
+getMessages();
